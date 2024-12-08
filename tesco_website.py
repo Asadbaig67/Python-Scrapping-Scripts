782805cc5bba19e0802aa3066dadc2ef98ad3fb3
Asadbaig67 <2020cs667@student.uet.edu.pk>
2024-12-08 15:44:43 +0500

    Initial commit

--- tesco_website.py
+++ tesco_website.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
 import time
 
 
--- waitrose_website_scrapping_new.py
+++ waitrose_website_scrapping_new.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
 import time
 
 
